fc2d41db3137a287bccf21c62915e9ced5610b43
Kevin Espitia <kedav.e23@outlook.com>
2024-03-11 20:18:56 -0500

    :art: Se agregaron nuevos comoponetes

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import LandingHeader from "@/components/LandingHeader";
+import MainSection from "@/components/MainSection";
+import SecondSection from "@/components/SecondSection";
 // import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <LandingHeader />
+      <main className="relative w-full h-screen overflow-auto snap-y snap-mandatory">
+        <div className="snap-start">
+          <MainSection />
+        </div>
+        <div className="snap-start">
+          <SecondSection/>
+        </div>
+      </main>
     </>
   );
 }
