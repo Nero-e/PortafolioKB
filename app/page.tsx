8844ab365ecebf53f5fa42ff9218593bfca881d2
Kevin Espitia <kedav.e23@outlook.com>
2024-03-11 19:37:10 -0500

    :art: Se agrego un componente header

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import LandingHeader from "@/components/LandingHeader";
+// import Image from "next/image";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <LandingHeader />
+    </>
+  );
 }
